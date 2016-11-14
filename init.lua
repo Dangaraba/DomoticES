e705bc0665d24f2f8c3d48b44e1136e7fa1a349d
Dangaraba <Dangaraba@gmail.com>
2016-11-14 02:27:33 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,12 +3,11 @@ wifi.sta.config("WLAN-SSID","WLAN-password")
 wifi.sta.setip({ip="192.168.1.101",netmask="255.255.255.0",gateway="192.168.1.1"})
 
 --wait a second for the connection to succeed
-
 tmr.delay(1000000)
 
 print(wifi.sta.getip())
 
- -- Start a simple http server
+--Start a simple http server
 srv=net.createServer(net.TCP)
 srv:listen(80,function(conn)
   conn:on("receive",function(conn,payload)
