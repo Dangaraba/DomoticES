ce155522bda79f340dadc0cf4f59990a42c6bf0e
Dangaraba <Dangaraba@gmail.com>
2016-11-14 02:25:15 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,18 @@
 wifi.setmode(wifi.STATION)
 wifi.sta.config("WLAN-SSID","WLAN-password")
+wifi.sta.setip({ip="192.168.1.101",netmask="255.255.255.0",gateway="192.168.1.1"})
+
 --wait a second for the connection to succeed
+
 tmr.delay(1000000)
-print(wifi.sta.status())
+
 print(wifi.sta.getip())
-print(wifi.sta.getmac())
+
+ -- Start a simple http server
+srv=net.createServer(net.TCP)
+srv:listen(80,function(conn)
+  conn:on("receive",function(conn,payload)
+    print(payload)
+    conn:send("<h1>DomoticES</h1>") end)
+  conn:on("sent",function(conn) conn:close() end)
+end)
